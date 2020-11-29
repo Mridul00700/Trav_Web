a11542282c94397b6de40aaa37cc918e6113cc76
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2020-11-29 22:15:16 +0530

    initial_finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Signup from './components/pages/Signup';
+import Services from './components/pages/Services';
+
 function App() {
   return (
     <Router>
@@ -10,6 +14,9 @@ function App() {
       <Navbar/>
       <Switch> 
         <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/sign-up" exact component={Signup} />
       </Switch>
 
       </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,15 @@ import '../../App.css';
 import Main from '../main';
 import React from 'react';
 import Cards from '../cards';
+import Footer from '../Footer';
 
 function Home() {
     return(
     <>
         <Main/>
         <Cards/>
+        <Footer/>
+
     </>
     );
 }
